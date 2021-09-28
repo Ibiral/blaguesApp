188386a4fe57a0caf220e8db61fa4513ee5feb17
Ibiral <tnt_djurdjura@hotmail.fr>
2021-09-28 16:20:58 +0200

    feat: logique du bouton

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const blagueHeader = document.getElementById("b_header");
 const blagueContent = document.getElementById("b_content");
+const blagueBtn = document.getElementById("b_btn");
 
 function getBlague() {
   fetch("https://api.blablagues.net/?rub=blagues")
     .then((res) => res.json())
     .then((data) => {
       const content = data.data.content;
-      console.log(content);
 
       blagueHeader.textContent = content.text_head;
       blagueContent.textContent =
@@ -16,4 +16,4 @@ function getBlague() {
 
 getBlague();
 
-document.body.addEventListener("click", getBlague);
+blagueBtn.addEventListener("click", getBlague);
